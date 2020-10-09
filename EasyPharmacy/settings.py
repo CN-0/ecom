c4cde160fd7e75b0cd2cb6e73c9c07abc991370b
unknown <codingnoob0@gmail.com>
2020-10-09 16:02:18 +0530

    main

--- EasyPharmacy/settings.py
+++ EasyPharmacy/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51HSyXaHRX19RFHUQy2n8zVHJHimdnVICpNq2w295LQ3jeDZKw4ap1Pad2WWqS1sXe0o21Wq5Dbb31gqUVhBePeEX00Fq9IxS3y'
